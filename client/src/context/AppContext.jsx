0c0908b96b5cf9cf7fd332c5d2f87c1ed23b62a4
Paulongf <paulongf@hotmail.com>
2025-06-05 12:07:22 +0100

    Creating NewsLetter, BestSellers, Footer at Home

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -23,7 +23,7 @@ export const AppContextProvider = ({children})=>{
     }
 
     // Add Product to Cart
-    const addToCart = ()=>{
+    const addToCart = (itemId)=>{
         let cartData = structuredClone(cartItems);
 
         if(cartData[itemId]){
