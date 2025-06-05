bc2e5de237491b55b813f9a8e80d0b99c8a6da66
Paulongf <paulongf@hotmail.com>
2025-06-05 16:02:29 +0100

    Implementing ProductDetails

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
 
     const [open, setOpen] = React.useState(false);
-    const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery} = useAppContext();
+    const {user, setUser, setShowUserLogin, navigate, 
+        setSearchQuery, searchQuery, getCartCount
+    } = useAppContext();
 
 
     const logout = async ()=>{
@@ -41,7 +43,9 @@ const Navbar = () => {
 
                 <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
+                        {getCartCount()}
+                    </button>
                 </div>
 
                 {!user ? (<button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
@@ -61,10 +65,19 @@ const Navbar = () => {
                 }
             </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-                {/* Menu Icon SVG */}
-                <img src={assets.menu_icon} alt='menu' className=''/>
-            </button>
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
+                        {getCartCount()}
+                    </button>
+                </div>
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
+                    {/* Menu Icon SVG */}
+                    <img src={assets.menu_icon} alt='menu' className=''/>
+                </button>
+            </div>
+            
 
             {/* Mobile Menu */}
            { open && (
