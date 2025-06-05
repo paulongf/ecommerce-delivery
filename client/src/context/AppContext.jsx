6c6f55bfc871604b25c87f766ddb390f6dac48b7
Paulongf <paulongf@hotmail.com>
2025-06-05 14:58:02 +0100

    Implementing Filter Products

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -16,6 +16,7 @@ export const AppContextProvider = ({children})=>{
     const [products, setProducts] = useState([]);
 
     const [cartItems, setCartItems] = useState({});
+    const [searchQuery, setSearchQuery] = useState({});
 
     // Fetch All Products
     const fetchProducts = async ()=> {
@@ -63,7 +64,8 @@ export const AppContextProvider = ({children})=>{
 
     const value = {navigate, user, setUser, isSeller, 
         setIsSeller, showUserLogin, setShowUserLogin, products,
-        currency, addToCart, updateCartItem, removeFromCart, cartItems
+        currency, addToCart, updateCartItem, removeFromCart, cartItems,
+        searchQuery, setSearchQuery
     };
 
     return <AppContext.Provider value={value}>
